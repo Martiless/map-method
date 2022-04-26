101b3546a20b9a25dbb5155221294c9f759d1efa
Martina Martin <marti.law@gmail.com>
2022-04-26 08:50:53 +0000

    Add studentsAsObjects

--- map.js
+++ map.js
@@ -4,28 +4,28 @@
  * command node map.js in the terminal
  */
 
-// Using a for loop
-let nums = [1,2,3,4,5];
-let results = [];
-for (let num of nums) {
-  results.push(num *2);
-}
-console.log(results);
+// // Using a for loop
+// let nums = [1,2,3,4,5];
+// let results = [];
+// for (let num of nums) {
+//   results.push(num *2);
+// }
+// console.log(results);
 
-// Using map()
-const multByTwo = function (num) {
-  return num *2;
-}
-const mapResults = nums.map(multByTwo);
-console.log(mapResults)
+// // Using map()
+// const multByTwo = function (num) {
+//   return num *2;
+// }
+// const mapResults = nums.map(multByTwo);
+// console.log(mapResults)
 
-// Simplified w/ map()
-const mapResult2 = nums.map(function (num) {return num *2});
-console.log(mapResult2)
+// // Simplified w/ map()
+// const mapResult2 = nums.map(function (num) {return num *2});
+// console.log(mapResult2)
 
-// Simplfied w/ map() + arrow function
-const arrow = nums.map(num => num *2 );
-console.log(arrow)
+// // Simplfied w/ map() + arrow function
+// const arrow = nums.map(num => num *2 );
+// console.log(arrow)
 
 // With objects:
 const students = [
@@ -51,3 +51,6 @@ const students = [
 
 const studentsWithIds = students.map(students => [students.name, students.id]);
 console.log(studentsWithIds)
+
+const studentsAsObject = students.map(students => ({id: students.id, name: students.name}));
+console.log(studentsAsObject)
